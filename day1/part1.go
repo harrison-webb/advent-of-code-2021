c6269ffc7daeccaad74dfb83c4f48496320f759c
harrison-webb <88354435+harrison-webb@users.noreply.github.com>
2021-12-01 07:10:45 +0000

    Initial commit

---
+++ day1/part1.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+   "bufio"
+    "fmt"
+    "log"
+    "strconv"
+    "os"
+)
+
+func main(){
+  // open file
+    f, err := os.Open("aocDay1Input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // remember to close the file at the end of the program
+    defer f.Close()
+
+    // read the file line by line using scanner
+    scanner := bufio.NewScanner(f)
+
+    var prevMeasurement int = 100000;
+    var numGreater = 0;
+    for scanner.Scan() {
+        // do something with a line scanner.Text()
+        //check if current > prevMeasurement
+        currentMeasurement, err := strconv.Atoi(scanner.Text())
+        _ = err
+        if currentMeasurement > prevMeasurement{
+          numGreater++;
+        }
+        prevMeasurement = currentMeasurement
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println(numGreater)
+
+}
---
+++ day1/part2.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+   "bufio"
+    "fmt"
+    "log"
+    "strconv"
+    "os"
+)
+
+func main(){
+  results, _ := ReadInts("aocDay1Input.txt")
+  numGreater := 0
+  var window1 int = results[0] + results[1] + results[2]
+  var window2 int
+  for i := 2; i < len(results) - 1; i++{
+    window2 = results[i-1] + results[i] + results[i+1]
+    if window2 > window1{
+      numGreater++
+    }
+    window1 = window2
+  }
+  fmt.Println(numGreater)
+}
+
+func ReadInts(fname string) ([]int, error) {
+  f, err := os.Open(fname)
+    if err != nil {
+        log.Fatal(err)
+    }
+  defer f.Close()
+  
+  scanner := bufio.NewScanner(f)
+  var result []int
+  for scanner.Scan() {
+      x, err := strconv.Atoi(scanner.Text())
+      if err != nil {
+          return result, err
+      }
+      result = append(result, x)
+  }
+  return result, scanner.Err()
+}
